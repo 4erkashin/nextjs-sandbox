2102cc86fea2b4243d3953c34e1de79678f2f649
Iurii Cherkashin <cherkashin.ju@gmail.com>
2025-09-12 13:57:45 +0200

    refactor: update global styles and layout
    - Replaced font variables in globals.css to use Poppins instead of Geist.
    - Updated color variables for improved design consistency.
    - Simplified layout in page.tsx, removing unnecessary elements and adding a basic "Hello World" header.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Poppins } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 const geistMono = Geist_Mono({
@@ -25,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
