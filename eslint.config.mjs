5aa4a8c231703c7a47c0f09e1ec4f9ab92060f00
Iurii Cherkashin <cherkashin.ju@gmail.com>
2026-01-20 15:54:16 -0300

    chore: integrate @tanstack/react-query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
@@ -7,6 +8,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...pluginQuery.configs['flat/recommended'],
   perfectionist.configs["recommended-natural"],
   eslintConfigPrettier,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
