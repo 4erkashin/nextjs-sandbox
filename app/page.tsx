0d560096efd4bc0cf6c0ef7a41010343875df6c9
Iurii Cherkashin <cherkashin.ju@gmail.com>
2026-01-20 14:22:00 -0300

    chore: init shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <pre>login flow</pre>
-        <pre>{JSON.stringify({ 1: "shadcn", 2: "tanstack-query" }, null, 2)}</pre>
+        <pre>{JSON.stringify(["tanstack-query"], null, 2)}</pre>
       </main>
     </div>
   );
